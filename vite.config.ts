7c2a86adf1ff733b106828de6e3aa439b98dc517
tsots <mosalatsotetsi@gmail.com>
2026-08-14 22:14:39 +0200

    chaged config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,67 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+//
+// import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+//
+// import viteReact from '@vitejs/plugin-react'
+//   const isProd = mode === 'production';
+//   const BASE = isProd ? '/your-repo-name/' : '/';
+//
+// const config = defineConfig({
+//   resolve: { tsconfigPaths: true },
+//   preview: {
+//     host: '127.0.0.1',
+//   },
+//   plugins: [
+//     tanstackStart({
+//         router: {
+//           basepath: BASE,
+//         },
+//         client: {
+//           base: BASE,
+//         },
+//       prerender: {
+//         enabled: true,
+//         crawlLinks: true,
+//       },
+//     }),
+//     viteReact(),
+//   ],
+// })
+//
+// export default config
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 
-import viteReact from '@vitejs/plugin-react'
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  const BASE = isProd ? '/class_act_stack/' : '/';
 
-const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  preview: {
-    host: '127.0.0.1',
-  },
-  plugins: [
-    tanstackStart({
-      prerender: {
-        enabled: true,
-        crawlLinks: true,
-      },
-    }),
-    viteReact(),
-  ],
-})
+  return {
+    base: BASE,
 
-export default config
+    plugins: [
+      tsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+
+      tanstackStart({
+        router: {
+          basepath: BASE,
+        },
+        client: {
+          base: BASE,
+        },
+        prerender: {
+          enabled: true,
+          crawlLinks: true,
+        },
+      }),
+
+      react(),
+    ],
+  };
+});
